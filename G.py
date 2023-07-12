cee0cfe79d4147632798a177d69680dce3e7d80d
Hehdhr <117031830+Hehdhr@users.noreply.github.com>
2023-07-12 09:49:33 +0300

    Update G.py

--- G.py
+++ G.py
@@ -1,14 +1,18 @@
 
 import requests
 
-def search_users():
-    for i in range(1000, 10000):  # تبحث عن اليوزرات الرباعية من 1000 إلى 9999
-        username = str(i)
-        url = f"https://tiktok.com/@{username}"
-
-        response = requests.get(url)
-        if response.status_code == 404:
-            print(f"اليوزر {username} غير متاح.")
-        else:
-            print(f"اليوزر {username} متاح.")
+def search_tiktok_users():
+    base_url = "https://api.tiktok.com/"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36",
+        "Referer": "https://www.tiktok.com/"
+    }
+    
+    for username in range(1000, 10000):  # تبدأ بالرقم 1000 وتنتهي بالرقم 9999
+        url = f"{base_url}@{username}"
+        response = requests.get(url, headers=headers)
         
+        if response.status_code == 200:  # تحقق مما إذا كان الاستجابة ناجحة
+            print(f"تم العثور على مستخدم تيك توك: {username}")
+
+search_tiktok_users() 
