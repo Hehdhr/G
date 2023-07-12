ea2df8409641f9093a65e316633a83c35966651c
Hehdhr <117031830+Hehdhr@users.noreply.github.com>
2023-07-12 09:38:54 +0300

    Create G.py

---
+++ G.py
@@ -0,0 +1,15 @@
+
+import requests
+
+def search_users():
+    for i in range(1000, 10000):  # تبحث عن اليوزرات الرباعية من 1000 إلى 9999
+        username = str(i)
+        url = f"https://tiktok.com/@{username}"
+
+        response = requests.get(url)
+        if response.status_code == 404:
+            print(f"اليوزر {username} غير متاح.")
+        else:
+            print(f"اليوزر {username} متاح.")
+
+search_users() 
